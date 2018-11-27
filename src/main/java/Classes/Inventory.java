fa72f091db88e0d62833dd4fd81dee3dcada9dcb
Florian Quibel <florian.quibel27@gmail.com>
2018-11-27 14:45:05 +0100

    update gui

--- src/main/java/Classes/Inventory.java
+++ src/main/java/Classes/Inventory.java
@@ -32,12 +32,7 @@ public class Inventory {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if(items[i] instanceof Aged_Brie)  items[i].update((Aged_Brie) items[i]);
-            if(items[i] instanceof Backstage_passes)  items[i].update((Backstage_passes) items[i]);
-            if(items[i] instanceof Conjured_Cake)  items[i].update((Conjured_Cake) items[i]);
-            if(items[i] instanceof Dexterity)  items[i].update((Dexterity) items[i]);
-            if(items[i] instanceof Elixir)  items[i].update((Elixir) items[i]);
-            if(items[i] instanceof Sulfuras)  items[i].update((Sulfuras) items[i]);
+            items[i].updateQuality();
         }
     }
 
