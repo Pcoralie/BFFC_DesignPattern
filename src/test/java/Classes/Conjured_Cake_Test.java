56007a624ec64e7d0e3e52f9bc614d2075971ec2
BastianM <bastian.molard@gmail.com>
2018-12-07 18:02:38 +0100

    Ajout tests unitaires

---
+++ src/test/java/Classes/Conjured_Cake_Test.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Conjured_Cake_Test {
+
+    @Test
+    public void qualityCheck()
+    {
+
+    }
+}
