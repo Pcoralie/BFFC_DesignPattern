e35956c52bb2a04fe0f1dd50d5e200f76e7ab10b
BastianM <bastian.molard@gmail.com>
2018-12-07 18:02:38 +0100

    Ajout tests unitaires

---
+++ src/test/java/Classes/Backstage_passes_Test.java
@@ -0,0 +1,14 @@
+package Classes;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Backstage_passes_Test {
+    @Test
+    public void qualityCheck()
+    {
+
+    }
+}
---
+++ src/test/java/Classes/Inventory_Test.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Inventory_Test {
+
+    @Test
+    public void qualityCheck()
+    {
+
+    }
+}
